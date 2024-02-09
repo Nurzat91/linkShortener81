94546236ba5d6f793b37aa7b56f1a7aae2959850
Nurzat Taalaibekova <tnurzat91@gmail.com>
2024-02-09 11:05:26 +0300

    receiving a short record

--- api/routers/link.ts
+++ api/routers/link.ts
@@ -23,4 +23,17 @@ linksRouter.post('/',  async (req, res, next) => {
   }
 });
 
+linksRouter.get('/:shortUrl', async (req, res, next) => {
+  try {
+    const link = await Links.findOne({ shortUrl: req.params.shortUrl });
+
+    if (!link) {
+      return res.status(404).json({ error: 'Link not found' });
+    }
+    res.status(301).redirect(link.link);
+  } catch (e) {
+    next(e);
+  }
+});
+
 export default linksRouter;
--- frontend/src/containers/Home/Home.tsx
+++ frontend/src/containers/Home/Home.tsx
@@ -6,6 +6,7 @@ const Home = () => {
   const [text, setText] = useState({
     link: '',
   });
+  const [shortUrl, setShortUrl] = useState('');
 
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>{
     const {name, value} = e.target;
@@ -18,36 +19,48 @@ const Home = () => {
   const formSubmitHandler = (e: React.FormEvent) => {
     e.preventDefault();
 
-    console.log('Link');
+    const shortened = `http://localhost:8000/${text.link}`;
+    setShortUrl(shortened);
+
     setText({ link: '' });
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <Grid container direction="column" sx={{mb: 3}}>
+    <>
+      <form onSubmit={formSubmitHandler}>
+        <Grid container direction="column" sx={{mb: 3}}>
+          <Card sx={{ textAlign: 'center', padding: 2 }}>
+            <Typography variant="h4" sx={{mb: 2}}>Shorten your link!</Typography>
+            <Grid item xs sx={{m: 2}}>
+              <TextField
+                required
+                id="link" label="Link"
+                name="link"
+                value={text.link}
+                onChange={inputChangeHandler}
+              />
+            </Grid>
+            <Grid item xs sx={{m: 2}}>
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+              >
+                Shorten!
+              </Button>
+            </Grid>
+          </Card>
+        </Grid>
+      </form>
+      {shortUrl && (
         <Card sx={{ textAlign: 'center', padding: 2 }}>
-          <Typography variant="h4" sx={{mb: 2}}>Shorten your link!</Typography>
-          <Grid item xs sx={{m: 2}}>
-            <TextField
-              required
-              id="link" label="Link"
-              name="link"
-              value={text.link}
-              onChange={inputChangeHandler}
-            />
-          </Grid>
-          <Grid item xs sx={{m: 2}}>
-            <Button
-              type="button"
-              color="primary"
-              variant="contained"
-            >
-              Shorten!
-            </Button>
-          </Grid>
+          <Typography variant="h5">
+            Your link now looks like this:
+          </Typography>
+          <a href={shortUrl} target="_blank" rel="shortUrl">{shortUrl}</a>
         </Card>
-      </Grid>
-    </form>
+      )}
+    </>
   );
 };
 
